37bdf4441ed22ce6be776e1bca83f7250024b39c
Izzbot <izzbot@mindspring.com>
2014-10-29 12:16:57 -0700

    continued

--- js/app.js
+++ js/app.js
@@ -1,54 +1,27 @@
 
+
+// Set up the necessary variables
+var tiles = [];
+var matched = 0;
+var misses = 0;
+var currentImg;
+
 // Let's start by running after the DOM is ready
 $(document).ready(function() {
 
-    // Set up the necessary riables
-    var tiles = [];
     var i;
-    var solved = false;
 
     // Load up the tile array
     for (i = 1; i <= 32; i++) {
         tiles.push({
            num: i,
            src: 'img/tile' + i + '.jpg',
-           flipped: true
+           flipped: false,
+           matched: false
         });
     }
 
-    // Shuffle up choose eight of them
-    var selectedTiles = _.shuffle(tiles).slice(0,8);
-
-    // Clone our set tiles to pair them up
-    var tilePairs = [];
-    _.forEach(selectedTiles, function(tile) {
-        tilePairs.push(_.clone(tile));
-        tilePairs.push(_.clone(tile));
-    });
-
-    // Shuffle it up
-    tiles = _.shuffle(tilePairs);
-
-    // Build our game board
-    var board = $('#game-board');
-    var row = $(document.createElement('div'));
-    var img;
-    _.forEach(tiles, function(tile, elemIndex) {
-        if (elemIndex > 0 && 0 == elemIndex % 4) {
-            board.append(row);
-            row = $(document.createElement('div'));
-        }
-
-       img = $(document.createElement('img'));
-       img.attr({
-            src: tile.src,
-            alt: 'Image ' + tile.num
-       });
-
-        img.data('tile', tile);
-        row.append(img);
-    });
-    board.append(row);
+    buildGameboard();
 
     // Build the game functionality
     $('#game-board img').click(function() {
@@ -73,10 +46,51 @@ $(document).ready(function() {
         $('elapsed-seconds').text('Time Elapsed: ' + elapsed);
 
         // Stop the timer when necessary
-        if (solved) {
+        if (matched = 8) {
             window.clearInterval(timer);
         }
     },1000);
 
 
 });
+
+
+function buildGameboard() {
+    // Shuffle up choose eight of them
+    var selectedTiles = _.shuffle(tiles).slice(0,8);
+
+    // Clone our set tiles to pair them up
+    var tilePairs = [];
+    _.forEach(selectedTiles, function(tile) {
+        tilePairs.push(_.clone(tile));
+        tilePairs.push(_.clone(tile));
+    });
+
+    // Shuffle it up
+    var gameTiles = _.shuffle(tilePairs);
+
+    // Build our game board
+    var board = $('#game-board');
+    var row = $(document.createElement('div'));
+    var img;
+    _.forEach(gameTiles, function(tile, elemIndex) {
+        if (elemIndex > 0 && 0 == elemIndex % 4) {
+            board.append(row);
+            row = $(document.createElement('div'));
+        }
+
+        img = $(document.createElement('img'));
+        img.attr({
+            src: 'img/tile-back.png',
+            alt: 'Image ' + tile.num
+        });
+
+        img.data('tile', tile);
+        row.append(img);
+    });
+    board.append(row);
+
+    matched = 0;
+    misses = 0;
+    currentImg = 0;
+}
