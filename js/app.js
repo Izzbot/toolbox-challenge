0b884f2d88e010c3c63204ecefdb01c2a83cb77f
Izzy <izzbot@mindspring.com>
2014-11-04 17:03:11 -0800

    half done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // Set up the necessary variables
 var tiles = [];
-var matched = 0;
+var matches = 0;
 var misses = 0;
 var currentImg;
 
@@ -10,6 +10,7 @@ var currentImg;
 $(document).ready(function() {
 
     var i;
+    var startTime = _.now();
 
     // Load up the tile array
     for (i = 1; i <= 32; i++) {
@@ -20,13 +21,83 @@ $(document).ready(function() {
            matched: false
         });
     }
-
+	
+	// Make the game board!
     buildGameboard();
 
     // Build the game functionality
     $('#game-board img').click(function() {
        var img = $(this);
        var tile = img.data('tile');
+		
+		// Check to see if it's a legal choice
+       if (!tile.flipped) {
+	
+			// Flip the image
+			flipImage(img);
+
+			// See if this is the second image selected
+	   		if (!currentImg == 0) {
+	   		
+	   			// Check for a match
+	   			if (tile.num == currentImg.data('tile').num) {
+	   				tile.matched = true;
+	   				currentImg.data('tile').matched = true;
+					matches++;   			
+
+	   			// No match
+	   			} else {
+
+//  NEED TO ADD A DELAY OF 1000ms HERE
+
+					flipImage(img);
+					flipImage(currentImg);					
+					misses++;
+	   			}
+	   			
+				currentImg = 0;
+
+	   		// Otherwise, set it up as the first selected image	
+	   		} else {
+	   			currentImg = img;		
+	   		}        
+       }        
+
+        console.log(elapsed);
+    });
+
+
+    // Build the timers
+    var timer = window.setInterval(function() {
+		var elapsed = 20;
+//		var elapsed = moment().diff(startTime, 'seconds');
+//        var elapsed = Math.floor(_.now() - startTime / 1000);
+        $('elapsed').text('Time Elapsed: ' + elapsed);
+        $('matches').text('Matches: ' + matches);
+        $('remaining').text('Remaining: ' + (8 - matches));
+        $('misses').text('Misses: ' + misses);
+
+        // Stop the timer when necessary
+        if (matches == 8) {
+            window.clearInterval(timer);
+            winGame();
+        }
+    },1000);
+
+
+	// Set up the new game button
+	$('#newGameButton').click(function() {
+		buildGameboard();
+	});
+});
+
+
+/*
+ * Flips the selected image/tile
+ */
+function flipImage(img) {
+		var tile = img.data('tile');
+
         img.fadeOut(100, function() {
             if (tile.flipped) {
                 img.attr('src', 'img/tile-back.png');
@@ -34,28 +105,24 @@ $(document).ready(function() {
                 img.attr('src', tile.src);
             }
             tile.flipped = !tile.flipped;
-            img.fadeIn(100);
+            img.fadeIn(100);       	
         });
-    });
+}
 
+/*
+ * Display Victory
+ */
+function winGame() {
+	console.log('YOU HAVE WON THE GAME');
+}
 
-    // Build the timers
-    var startTime = _.now();
-    var timer = window.setInterval(function() {
-        var elapsed = Math.floor((_.now() - startTime) / 1000);
-        $('elapsed-seconds').text('Time Elapsed: ' + elapsed);
-
-        // Stop the timer when necessary
-        if (matched = 8) {
-            window.clearInterval(timer);
-        }
-    },1000);
-
-
-});
-
-
+/*
+ * Builds a new Game
+ */
 function buildGameboard() {
+	// Clear the existing board
+	$('#game-board').empty();
+
     // Shuffle up choose eight of them
     var selectedTiles = _.shuffle(tiles).slice(0,8);
 
@@ -90,7 +157,7 @@ function buildGameboard() {
     });
     board.append(row);
 
-    matched = 0;
+    matches = 0;
     misses = 0;
     currentImg = 0;
 }
